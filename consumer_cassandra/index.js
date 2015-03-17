7e68980cade2cdf6c9b400bf8cf4efee9c11bf25
Chris Thompson <chris@build3software.com>
2015-03-17 09:46:37 -0500

    not sure

--- consumer_cassandra/index.js
+++ consumer_cassandra/index.js
@@ -2,19 +2,25 @@
  * Created by chris on 3/16/15.
  */
 var cassandra = require('cassandra-driver');
-var client = new cassandra.Client({contactPoints: ['192.168.59.103'], keyspace:'ks1'});
+var client = new cassandra.Client({contactPoints: ['192.168.59.103'], keyspace:'ks1', port: 9042});
 var query = 'insert into test(key) values (test1)';
-client.execute(query, undefined, {prepare:false}, function(err){
-   if (err){
-       console.log(err);
-       return;
-   }
-    console.log('inserted');
-    client.eachRow('select * from test', undefined, function(n, row){
-        console.log('row val is ' + row.key);
-    }, function(err){
-        if (err){
-            console.log('an error occurred ' + err);
-        }
-    });
+//client.execute(query, undefined, {prepare:false}, function(err){
+//   if (err){
+//       console.log(err);
+//       return;
+//   }
+//    console.log('inserted');
+//    client.eachRow('select * from test', undefined, function(n, row){
+//        console.log('row val is ' + row.key);
+//    }, function(err){
+//        if (err){
+//            console.log('an error occurred ' + err);
+//        }
+//    });
+//});
+client.connect(function(err){
+    if (err){
+        console.log("error " + err);
+    }
+    console.log("connected");
 });
